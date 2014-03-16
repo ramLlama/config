6b608e3b3ce214cbc12df1e80c7f86088a51358f
Ram Raghunathan <ram@llama.is>
2014-03-16 16:05:37 -0400

    config.fish: Fix singleton ssh-agent

--- config.fish
+++ config.fish
@@ -25,17 +25,19 @@ if status --is-interactive
     # Start, if necessary, ssh-agent
     # Stolen from https://gist.github.com/daniel-perry/3251940
     set -x SSH_AUTH_SOCK {$HOME}/.ssh/ssh-agent-socket
-    set NKEYS_SSHAGENT (ssh-add -l 2> /dev/null | wc -l)
-    if test {$NKEYS_SSHAGENT} = 0
-	# No ssh-agent running
+    ssh-add -l ^ /dev/null > "$TEMP_FILE"
+    set SSH_ADD_STATUS $status
+    set SSH_AGENT_NUM_KEYS (cat "$TEMP_FILE" | wc -l)
+    if test \( {$SSH_ADD_STATUS} != 0 \) -o \( {$SSH_AGENT_NUM_KEYS} = 0 \)
+	# kill ssh-agent and start from a clean slate
+	killall ssh-agent ^ /dev/null
 	rm -f {$SSH_AUTH_SOCK}
 	ssh-agent -a {$SSH_AUTH_SOCK} -c | \
 	    sed 's/^setenv/set -x/' | \
-	    sed 's/^echo/#echo/' > /tmp/.ssh-script
-	. /tmp/.ssh-script
+	    sed 's/^echo/#echo/' > "$TEMP_FILE"
+	. "$TEMP_FILE"
 	echo $SSH_AGENT_PID > $HOME/.ssh/ssh-agent.pid
-	rm -f /tmp/.ssh-script
-	ssh-add {$HOME}/.ssh/id_rsa
+	ssh-add
     end
 
     # Set EDITOR to editor
