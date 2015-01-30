00e7a6e4dd5f88cc712dda2b392bbc5f623a92d6
Ram Raghunathan <ram.r@cs.cmu.edu>
2015-01-29 20:47:26 -0500

    fish: consolidate fish config, add solarized.fish

--- fish/config.fish
+++ fish/config.fish
@@ -80,3 +80,8 @@ set -x PATH (echo $PATH | tr ' ' '\n' | perl -ne 'BEGIN { %seen = (); } chomp $_
 # Remove temp file #
 ####################
 rm "$TEMP_FILE"
+
+##################
+# Load Solarized #
+##################
+. ./solarized.fish
