3d81b70c660edf115e679d757907c0f1de3fbef7
Ram Raghunathan <ram@Rams-Air.local>
2023-12-03 22:28:20 -0800

    fish: formatting

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -131,15 +131,14 @@ if status --is-interactive
     set SSH_ADD_STATUS $status
     set SSH_AGENT_NUM_KEYS (cat "$TEMP_FILE" | wc -l)
     if test \( {$SSH_ADD_STATUS} != 0 \) -o \( {$SSH_AGENT_NUM_KEYS} = 0 \)
-	# kill ssh-agent and start from a clean slate
-	killall ssh-agent 2> /dev/null
-	rm -f {$SSH_AUTH_SOCK}
-	ssh-agent -a {$SSH_AUTH_SOCK} -c | \
-	    sed 's/^setenv/set -x/' | \
-	    sed 's/^echo/#echo/' > "$TEMP_FILE"
-	. "$TEMP_FILE"
-	echo $SSH_AGENT_PID > $HOME/.ssh/ssh-agent.pid
-	ssh-add
+    	# kill ssh-agent and start from a clean slate
+	    killall ssh-agent 2> /dev/null
+	    rm -f {$SSH_AUTH_SOCK}
+	    ssh-agent -a {$SSH_AUTH_SOCK} -c | \
+	              sed 's/^setenv/set -x/' > "$TEMP_FILE"
+	    . "$TEMP_FILE"
+	    echo $SSH_AGENT_PID > $HOME/.ssh/ssh-agent.pid
+	    ssh-add
     end
 end
 
